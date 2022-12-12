8c1445d81761e1a987772817b181564d365ce2c3
nguyentamthanh <thanhh16012k@gmail.com>
2022-12-12 09:14:14 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
   </div>
 </template>
 <script setup>
+import { shuffled } from "../utils/array"
 const statusMatch = ref("default")
 const settings = ref({
   totalOfBlocks: 0,
@@ -13,8 +14,12 @@ const settings = ref({
 })
 const time = ref(0)
 function onHandleBeforeStart(config) {
-  console.log("🚀 ~ file: index.vue:16 ~ onHandleBeforeStart ~ config", config)
   statusMatch.value = 'match'
   settings.value.totalOfBlocks = config.totalOfBlocks
+  const firstCards = Array.from({ length: settings.value.totalOfBlocks / 2 }, (_, i) => i + 1)
+  const secondCards = [...firstCards]
+  const cards = [...firstCards, ...secondCards]
+  settings.value.cardsContext = shuffled(shuffled(shuffled(shuffled(cards))))
+  console.log("🚀 ~ file: index.vue:23 ~ onHandleBeforeStart ~ settings.value.cardsContext", settings.value.cardsContext)
 }
 </script>
